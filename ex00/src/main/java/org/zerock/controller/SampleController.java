b00c3a10c45146d74e53814ea8fbc5c787954791
shj89 <shj89@DESKTOP-TNTB9AJ>
2020-11-08 12:31:42 +0900

    initBinder : 데이터 서버로 인입시 체크

--- ex00/src/main/java/org/zerock/controller/SampleController.java
+++ ex00/src/main/java/org/zerock/controller/SampleController.java
@@ -1,14 +1,20 @@
 package org.zerock.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.zerock.domain.SampleDTO;
+import org.zerock.domain.SampleDTOList;
+import org.zerock.domain.TodoDTO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -16,6 +22,14 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/sample/*")
 @Log4j
 public class SampleController {
+	
+	//들어오는 데이터 검증
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(dateFormat, false));
+	}
+	
 	@RequestMapping("")
 	public void basic() {
 		log.info("basic............");
@@ -60,5 +74,19 @@ public class SampleController {
 		log.info("array ids : " + Arrays.toString(ids));
 		
 		return "ex02Array";
+	}
+	
+	@GetMapping("/ex02Bean")
+	public String ex02Bean(SampleDTOList list) {
+		log.info("list dtos: " + list);
+		
+		return "ex02Bean";
+	}
+	
+	@GetMapping("/ex03")
+	public String ex03(TodoDTO todo) {
+		log.info("todo : " + todo);
+		
+		return "ex03";
 	}
 }
